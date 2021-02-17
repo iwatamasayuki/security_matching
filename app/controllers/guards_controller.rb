5fbc2686604f1d93783272c26fc58fb7e527b3e7
iwatamasayuki <sorhoun3@gmail.com>
2021-02-17 11:47:21 +0900

    編集及び削除機能実装

--- app/controllers/guards_controller.rb
+++ app/controllers/guards_controller.rb
@@ -1,7 +1,9 @@
 class GuardsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_guard, only: [:edit, :show, :update, :destroy]
 
   def index
-    @guards = Guard.all
+    @guards = Guard.all.order('created_at DESC')
   end
 
   def new
@@ -19,12 +21,31 @@ class GuardsController < ApplicationController
   end
 
   def show
-    @guard = Guard.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @guard.update(guard_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @guard.destroy
+    redirect_to root_path
   end
 
   private
 
   def guard_params
     params.require(:guard).permit(:company, :image, :guard_name, :name_kana, :gender, :age, :experience, :qualification).merge(user_id: current_user.id)
+  end
+
+  def set_guard
+    @guard = Guard.find(params[:id])
   end
 end
